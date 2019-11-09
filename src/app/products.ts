8cfba20ff90a3af0e93eebbce93fcaa548e60726
LeeMing <leeming0619@hotmail.com>
2019-11-09 15:25:24 +0800

    upload app modules

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Phone',
+    price: 300,
+    description: "It's the best phone call I've ever had."
+  },
+  {
+    name: 'TV',
+    price: 200,
+    description: "It's a TV that no one can watch."
+  },
+  {
+    name: 'Apple',
+    price: 3,
+    description: 'Apple is very delicious..'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
